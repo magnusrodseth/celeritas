4892b717840c706e9e93b46f9258acd20c849d80
Magnus Rødseth <magnus.rodseth@gmail.com>
2021-07-09 13:30:57 +0200

    update landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,20 +7,29 @@ const Index = () => {
     <div className="bg-yellow-400 opacity-90">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
         <h2 className="text-3xl tracking-wide text-gray-900 sm:text-4xl">
+          <span className="block text-md my-4 font-bold tracking-wide">
+            Categorizing and visualizing the world of data structures and
+            algorithms.
+          </span>
           <span className="block text-2xl my-4 tracking-wide font-light">
-            Celeritas is a Latin word, translated as &quot;swiftness&quot; or
-            &quot;speed&quot;. It is often given as the origin of the symbol c,
-            the universal notation for the speed of light in a vacuum, as
-            popularized in Albert Einstein&apos;s famous equation E = mc
-            <sup>2</sup>.
+            The word &quot;celeritas&quot; is of Latin origin, translated as
+            &quot;swiftness&quot; or &quot;speed&quot;. It is often given as the
+            origin of the symbol <em>c</em>, the universal notation for the
+            speed of light in a vacuum, as popularized in Albert Einstein&apos;s
+            famous equation{" "}
+            <em>
+              E = mc
+              <sup>2</sup>
+            </em>
+            .
           </span>
           <span className="block text-white font-extrabold">
             See something you like in the navigation bar?
           </span>
         </h2>
-        <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+        <div className="m-4 flex lg:mt-0 lg:flex-shrink-0">
           <div className="inline-flex rounded-md shadow">
-            <Link href="/data-structures/arrays">
+            <Link href="/data-structures/binary-trees">
               <a
                 className={classNames(
                   "inline-flex items-center justify-center px-5 py-3 border border-transparent",
@@ -28,12 +37,12 @@ const Index = () => {
                   "transition transform duration-500 ease-in-out"
                 )}
               >
-                Link 1
+                Binary Trees
               </a>
             </Link>
           </div>
           <div className="ml-3 inline-flex rounded-md shadow">
-            <Link href="/data-structures/linked-lists">
+            <Link href="#">
               <a
                 className={classNames(
                   "inline-flex items-center justify-center px-5 py-3 border border-transparent",
